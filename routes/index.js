5e7046a7f120b938d03f2711effce8f71dd6ba85
Antonio Postigo Bernal <apostigo@gmail.com>
2015-06-14 12:22:57 +0200

    Creditos

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Quiz 2015. Autor' });
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
