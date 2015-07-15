2536ae7993bb0d2f8cd7077a2bfc40707c18bfdc
Antonio Postigo Bernal <apostigo@gmail.com>
2015-07-16 00:02:55 +0200

    Editar preguntas

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: [] });
 });
 
+// Página de créditos
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Quiz 2015. Autor', errors: []  });
+});
+
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
 
@@ -17,6 +22,8 @@ router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/new',                  quizController.new);
 router.post('/quizes/create',              quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
+router.put('/quizes/:quizId(\\d+)',        quizController.update);
 
 module.exports = router;
 
