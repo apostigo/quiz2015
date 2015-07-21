ef053ea05d8f9e6f2b480513f17042b6d33706e1
Antonio Postigo Bernal <apostigo@gmail.com>
2015-07-22 01:32:25 +0200

    P2P_M9_OP3

--- controllers/stats_controller.js
+++ controllers/stats_controller.js
@@ -6,23 +6,10 @@ exports.statistics=function(req,res){
 
  // promise = require('bluebird'),
   
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> bbf19fb847cade0b869cd4a59cbc7a1a962f3782
   query1 = 'SELECT COUNT(*) as count FROM "quizzes"',
   query2 = 'SELECT COUNT(*) as count FROM "comments"';
   query3 = 'select count(*) as count from "quizzes" where "id" in (select "quizid" from "comments")';
   query4 = 'select "tema",count(*) as cuenta from "quizzes" group by "tema"';
-<<<<<<< HEAD
-=======
-  query1 = 'SELECT COUNT(*) as count FROM quizzes',
-  query2 = 'SELECT COUNT(*) as count FROM comments';
-  query3 = "select count(*) as count from quizzes where id in (select quizid from comments)";
-  query4 = "select tema,count(*) as cuenta from quizzes group by tema";
->>>>>>> db9b236824321d6075ed3cfdd6e06a4663fc156f
-=======
->>>>>>> bbf19fb847cade0b869cd4a59cbc7a1a962f3782
 
   var chain = new Sequelize.Utils.QueryChainer();
 
