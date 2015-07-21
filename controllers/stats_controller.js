db9b236824321d6075ed3cfdd6e06a4663fc156f
Antonio Postigo Bernal <apostigo@gmail.com>
2015-07-21 19:30:44 +0200

    P2P modulo 9 con opcional estadisticas

---
+++ controllers/stats_controller.js
@@ -0,0 +1,28 @@
+var Sequelize = require('sequelize');
+var models = require('../models/models.js');
+				  
+// GET /statistics
+exports.statistics=function(req,res){
+
+ // promise = require('bluebird'),
+  
+  query1 = 'SELECT COUNT(*) as count FROM quizzes',
+  query2 = 'SELECT COUNT(*) as count FROM comments';
+  query3 = "select count(*) as count from quizzes where id in (select quizid from comments)";
+  query4 = "select tema,count(*) as cuenta from quizzes group by tema";
+
+  var chain = new Sequelize.Utils.QueryChainer();
+
+  chain
+    .add(models.sequelize.query(query1, null, { raw: true, plain: true }))
+    .add(models.sequelize.query(query2, null, { raw: true, plain: true }))
+	.add(models.sequelize.query(query3, null, { raw: true, plain: true }))
+	.add(models.sequelize.query(query4, null, { raw: true }))
+    .run()
+    .success(function(results) {
+		res.render('statistics',{titulo:"ESTADISTICAS ",estadisticas:results,errors: []  }  ) 
+    }).error(function(err) {
+      console.log('oh no', err);
+    });
+
+ };
